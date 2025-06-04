9c56eb6519b5852a6d74a1e9864903bd77a6dd07
Bastien Pruvost <bastien.mory@gmail.com>
2025-06-04 23:44:11 +0200

    feat: configure Prettier for Tailwind CSS

--- web/prettier.config.js
+++ web/prettier.config.js
@@ -3,8 +3,10 @@
 /** @type {import('prettier').Config} */
 const config = {
   semi: false,
-  singleQuote: true,
+  singleQuote: false,
   trailingComma: 'all',
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindStylesheet: './src/styles/global.css',
 }
 
 export default config
